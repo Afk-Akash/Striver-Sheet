e55087f7b83c609f1a125aa25ada49836cb36f75
Akash Kumar Singh <79398219+Afk-Akash@users.noreply.github.com>
2022-10-05 15:24:08 +0530

    Time: 146 ms (37.10%), Space: 10.4 MB (15.83%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -2,19 +2,37 @@ class Solution {
 public:
     int minCost(int n, vector<int>& cuts) {
         sort(cuts.begin(), cuts.end());
+        cuts.insert(cuts.begin(), 0);
+        cuts.push_back(n);
         int N = cuts.size();
-        vector<vector<int>> dp(N+1, vector<int>(N+1, -1));
-        return fun(0, n, 0, N, cuts, dp);
+        vector<vector<int>> dp(N+1, vector<int>(N+1, 0));
+        // return fun(1, N-2, cuts, dp);
+        for(int last = 0; last <= N-2; last++){
+            for(int ind = last + 1; ind <= N-2; ind++) dp[ind][last] = 0;
+        }
+        
+        for(int ind = N - 2; ind >= 1; ind--){
+            for(int last = ind; last <= N-2; last++){
+                int mini = INT_MAX;
+                for(int k = ind; k <= last; k++){
+                    int cost = cuts[last+1] - cuts[ind -1] + dp[ind][k - 1] + dp[k + 1][last];
+                    mini = min (mini, cost);
+                }
+                dp[ind][last] = mini;
+            }
+        }
+        return dp[1][N - 2];
     }
     
-    int fun(int i, int j, int ind, int last, vector<int> &cut, vector<vector<int>> &dp){
-        if(ind == last) return 0;
+    int fun(int ind, int last, vector<int> &cut, vector<vector<int>> &dp){
+        if(ind > last) {
+            return 0;
+        }
         if(dp[ind][last] != -1) return dp[ind][last];
         
         int mini = INT_MAX;
-        for(int k = ind; k < last; k++){
-            int atCut = cut[k];
-            int cost = (j-i) + fun(i, atCut, ind, k, cut, dp) + fun(atCut, j, k+1, last, cut, dp);
+        for(int k = ind; k <= last; k++){
+            int cost = cut[last+1] - cut[ind-1] + fun(ind, k-1, cut, dp) + fun(k+1, last, cut, dp);
             mini = min(mini, cost);
         }
         
