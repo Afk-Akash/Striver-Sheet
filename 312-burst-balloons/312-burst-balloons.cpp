9eb22fe64d2a2f42042f156439505d6ce3ff01dc
Akash Kumar Singh <79398219+Afk-Akash@users.noreply.github.com>
2022-10-05 18:25:49 +0530

    Time: 653 ms (59.92%), Space: 10.4 MB (53.19%) - LeetHub

---
+++ 312-burst-balloons/312-burst-balloons.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxCoins(vector<int>& nums) {
+        nums.insert(nums.begin(), 1);
+        nums.push_back(1);
+        int n = nums.size();
+        vector<vector<int>> dp(n+1, vector<int> (n-1, -1));
+        return fun(1, n-2, nums, dp);
+    }
+    
+    int fun(int start, int end, vector<int> &nums, vector<vector<int>> &dp){
+        if(start > end) return 0;
+        if(dp[start][end] != -1) return dp[start][end];
+        int maxi = 0;
+        for(int i = start; i <= end; i++){
+            int coins = nums[start-1] * nums[i] * nums[end + 1] + fun(start, i - 1, nums, dp) + fun(i + 1, end, nums, dp);
+
+            maxi = max(maxi, coins);
+        }
+        return dp[start][end] = maxi;
+    }
+};
