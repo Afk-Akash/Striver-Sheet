1bfb66c26597aea2656daa894d0b52f6a544afb8
Akash Kumar Singh <79398219+Afk-Akash@users.noreply.github.com>
2022-10-09 19:09:24 +0530

    Time: 938 ms (7.80%), Space: 37.2 MB (17.85%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -9,28 +9,25 @@ public:
     
     int minCut(string s) {
         int n = s.size();
-        vector<vector<int>> dp(n+1, vector<int> (n+1, -1));
-        return fun(0, n - 1, s, dp) - 1;
+        vector<vector<int>> ddp(n+1, vector<int> (n+1, -1));
+        vector<int> dp(n+1, -1);
+        return fun(0, s, dp)-1;
         
     }
     
-    int fun(int i, int j, string &s, vector<vector<int>> &dp){
-        if(isPalindrome(s,i,j))return 1;
-        if(i > j) return 0;
-        if(i == j) return 1;
+    int fun(int i, string &s, vector<int> &dp){
+        if(i == s.size()) return 0;
         
-        if(dp[i][j] != -1) return dp[i][j];
+        if(dp[i] != -1) return dp[i];
         int mini = 1e7;
         
-        for(int ind = i; ind < j; ind++){
+        for(int ind = i; ind < s.size(); ind++){
             if(isPalindrome(s, i,ind)){
-                // int l = fun(i, ind, s, dp);
-                // int r = fun(ind+1, j, s, dp);
-                int cost = 1 + fun(ind + 1, j, s, dp);
+                int cost = 1 + fun(ind + 1,s, dp);
             
                 mini = min(mini, cost);
             }
         }
-        return dp[i][j] = mini;
+        return dp[i] = mini;
     }
 };
