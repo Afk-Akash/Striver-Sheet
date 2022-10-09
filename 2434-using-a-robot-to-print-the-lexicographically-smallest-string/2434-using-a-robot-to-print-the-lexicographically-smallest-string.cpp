53741182b27602d8ebc8f50b42a962bc87a764c1
Akash Kumar Singh <79398219+Afk-Akash@users.noreply.github.com>
2022-10-09 11:44:18 +0530

    Time: 188 ms (33.33%), Space: 29.8 MB (100.00%) - LeetHub

--- 2434-using-a-robot-to-print-the-lexicographically-smallest-string/2434-using-a-robot-to-print-the-lexicographically-smallest-string.cpp
+++ 2434-using-a-robot-to-print-the-lexicographically-smallest-string/2434-using-a-robot-to-print-the-lexicographically-smallest-string.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
+    char low (vector<int> &v){
+        for(int i = 0; i <= 25; i++){
+            if(v[i]!=0)return i + 'a';
+        }
+        return 'a';
+    }
     string robotWithString(string s) {
         stack< char > st;
-        map<char,int> mp;
-        for(auto &c: s)mp[c]++;
+        vector<int> freq(26,0);
+        for(auto &c: s)freq[c-'a']++;
         int n = s.size();
         string ans = "";
         
         for(int i = 0; i < n; i++){
             st.push(s[i]);
-            mp[s[i]]--;
-            if(mp[s[i]] == 0) mp.erase(s[i]);
-            // cout<<st.top()<<" "<<mp.begin()-> first<<mp.begin()->second<<endl;
-            while(st.size() and st.top() <= mp.begin() -> first){
+            freq[s[i]-'a']--;
+            while(st.size() and st.top() <= low(freq)){
                 
                 ans += st.top();
                 st.pop();
