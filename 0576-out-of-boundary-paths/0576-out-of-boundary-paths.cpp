45d9504602bc42e37a243252346e588e19a27556
Akash Kumar Singh <79398219+Afk-Akash@users.noreply.github.com>
2022-10-13 12:40:28 +0530

    Time: 23 ms (39.19%), Space: 9.8 MB (25.44%) - LeetHub

--- 0576-out-of-boundary-paths/0576-out-of-boundary-paths.cpp
+++ 0576-out-of-boundary-paths/0576-out-of-boundary-paths.cpp
@@ -2,11 +2,12 @@ class Solution {
     int mod = 1000000000 + 7;
 public:
     int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
-        vector<vector<vector<int>>> dp(m+1, vector<vector<int>> (n+1, vector<int>(maxMove+1, -1)));
-        return fun(startRow, startColumn, maxMove, m, n, dp)%mod;
+        vector<vector<vector<int>>> dp(m+2, vector<vector<int>> (n+2, vector<int>(maxMove+1, -1)));
+        return fun(startRow+1, startColumn+1, maxMove, m, n, dp)%mod;
+        // for(int i)
     }
     int fun(int i, int j, int move, int row, int col, vector<vector<vector<int>>> &dp){
-        if(i < 0 or i == row or j < 0 or j == col)return 1;
+        if(i == 0 or i > row or j == 0 or j > col)return 1;
         if(move == 0) return 0;
         if(dp[i][j][move] != -1 ) return dp[i][j][move];
         
