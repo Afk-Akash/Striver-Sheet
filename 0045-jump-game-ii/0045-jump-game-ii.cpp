4067b57d9b8d0b373ecbb3c37b055e8278f2f356
Akash Kumar Singh <79398219+Afk-Akash@users.noreply.github.com>
2022-10-17 12:08:27 +0530

    Time: 33 ms (64.80%), Space: 16.7 MB (51.07%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,32 +1,19 @@
 class Solution {
 public:
-    int fun(int ind, vector<int> &nums, vector<int> &dp){
-        if(ind >= nums.size() - 1) return 0;
-        
-        if(dp[ind] != -1) return dp[ind];
-        int ans = 1e7;
-        for(int i = 1; i <= nums[ind]; i++){
-            int jump = 1 + fun(ind + i, nums, dp);
-            ans = min(ans, jump);
-        }
-        return dp[ind] = ans;
-    }
-    
     int jump(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n+1, 0);
-        // return fun(0, nums, dp);
+        int n = nums.size(), step = nums[0], maxReach = nums[0], ans = 1;
+        if(n == 1) return 0;
         
-        for(int i = n - 2; i >= 0; i--){
-            int k = nums[i];
-            int mini = 1e4;
-            
-            for(int j = i+1; j < min(i+k+1,n); j++){
-                mini = min(dp[j] , mini);
+        for(int i = 1; i < n; i++){
+            step--;
+            maxReach = max(maxReach, i + nums[i]);
+            // if(maxReach >= n - 1) return ans;
+            if(step == 0 and i != n-1){
+                ans++;
+                step = maxReach - i;
             }
-            
-            dp[i] = mini+1;
         }
-        return dp[0];
+        
+        return ans;
     }
 };
