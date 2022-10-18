5c857a77c9b33e0363f315325ecf95326fd3e043
Akash Kumar Singh <79398219+Afk-Akash@users.noreply.github.com>
2022-10-18 10:22:34 +0530

    Time: 49 ms (18.29%), Space: 24.8 MB (49.16%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.cpp
+++ 0337-house-robber-iii/0337-house-robber-iii.cpp
@@ -10,31 +10,17 @@
  * };
  */
 class Solution {
-    
+    unordered_map<TreeNode*, int> dp;
 public:
-    int fun(TreeNode* node, bool canTake, map<TreeNode*, unordered_map<bool,int>> &dp){
-      if(node -> left == NULL and node -> right == NULL) return canTake ? node -> val : 0;  
-        
-        if(dp[node][canTake] > 0) return dp[node][canTake];
-        int npl = 0, npr = 0, pl = 0, pr = 0;
-        if(node -> left)
-            npl = fun(node -> left, true, dp);
-        if(node -> right)
-            npr = fun(node -> right, true, dp);
-        if(canTake and node -> left)
-            pl = fun(node -> left, false, dp);
-        if(canTake and node -> right)
-            pr = fun(node -> right, false, dp);
-        int p = 0, np = 0;
-        if(canTake)
-            p = (pl+pr)+ node -> val;
-        np = (npl+npr);
-        // cout<<p<<" "<<np<<endl;
-        return dp[node][canTake] = max(p, np);
-    }
-    
     int rob(TreeNode* root) {
-        map<TreeNode*, unordered_map<bool,int>> dp;
-        return fun(root, true, dp);
+        if(root == NULL ) return 0;
+        if(dp[root] > 0) return dp[root];
+        int val = 0;
+        if(root -> left)
+            val = rob(root -> left -> left) + rob(root -> left -> right);
+        if(root -> right)
+            val += rob(root -> right -> left) + rob(root -> right -> right);
+        
+        return dp[root] = max(val + root -> val, rob(root -> left) + rob(root -> right));
     }
 };
