a997005669a22ff39e6d42fbdd398f2956daa8e5
Akash Kumar Singh <79398219+Afk-Akash@users.noreply.github.com>
2022-10-18 21:36:59 +0530

    Time: 882 ms (5.03%), Space: 74 MB (6.41%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    int maxx = INT_MIN;
+public:
+    int fun(vector<int> &nums, int ind){
+        if(ind < 0) return 0;
+        int ans = nums[ind] + fun(nums, ind - 1);
+        cout<<ans<<" ";
+        maxx = max(ans, maxx);
+        return ans > 0 ? ans : 0;
+    }
+    int maxSubArray(vector<int>& nums) {
+       int n = nums.size();
+         fun(nums, n - 1);
+        return maxx;
+    }
+};
