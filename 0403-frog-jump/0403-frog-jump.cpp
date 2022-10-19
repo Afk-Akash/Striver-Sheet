4dd63d5671c858880398b3a3dd69ee526faba1cd
Akash Kumar Singh <79398219+Afk-Akash@users.noreply.github.com>
2022-10-19 22:25:30 +0530

    Time: 173 ms (77.27%), Space: 231.9 MB (22.98%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
     bool canCross(vector<int>& stones) {
-        int n = stones.size();
-        map<int,unordered_set<int> > mp;
-        mp[1] = {1};
-        for(int i = 1; i < n; i++){
-            int pos = stones[i];
-            for(auto &a : mp[pos]){
-                mp[pos + a].insert(a);
-                mp[pos + a-1].insert(a-1);
-                mp[pos + a+1].insert(a+1);
-            }
+        int n  = stones.size();
+        if(n == 2) return stones[1] - stones[0] == 1;
+        vector<vector<int>> dp(n, vector<int>(n+1, -1));
+        return fun(0, 0, stones, dp);
+    }
+    
+    bool fun(int ind, int jump, vector<int> &v, vector<vector<int>> &dp){
+        if(ind == v.size()-1) return true;
+        if(dp[ind][jump] != -1) return dp[ind][jump];
+        int k = jump;
+        for(int i = -1; i <= 1; i++){
+            k = jump + i;
+            if(k <= 0)continue;
+            int index = lower_bound(v.begin(), v.end(), v[ind]+k) - v.begin();
+            if(index <= v.size() - 1 and v[index] - v[ind] == k) if(fun(index, k, v, dp)) return dp[ind][jump]=true;
         }
-        if(mp[stones[n-1]].size()) return 1;
-        return false;
+        
+        return dp[ind][jump] = false;
     }
 };
