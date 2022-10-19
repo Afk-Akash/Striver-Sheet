b45622f64700cff9477f5bfb1a07e6cd8fcfa391
Akash Kumar Singh <79398219+Afk-Akash@users.noreply.github.com>
2022-10-19 17:12:14 +0530

    Time: 17 ms (62.78%), Space: 7.5 MB (82.17%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        set<long long> set;
-        set.insert(-1);
-        int k = 0, val = 1;
-        while(set.size() and k < n){
-            auto it = set.end();
-            it--;
-            val = -(*it);
-            set.erase(-val);
-            set.insert(1LL*-1*2*val);
-            set.insert(1LL*-1*3*val);
-            set.insert(1LL*-1*5*val);
-            k++;
+        
+        vector<int> dp(n,0);
+        dp[0] = 1;
+        int l2 = 0, l3 = 0, l5 = 0;
+        for(int i = 1; i < n; i++){
+            dp[i] = min(dp[l2]*2, min(dp[l3]* 3, dp[l5] * 5));
+            if(dp[i] == dp[l2] * 2)l2++;
+            if(dp[i] == dp[l3] * 3)l3++;
+            if(dp[i] == dp[l5] * 5)l5++;
 
         }
-        
-        return val;
+        return dp[n-1];
     }
 };
