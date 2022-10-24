5fb8f5d450eb0e49458d5a36d32523081566c538
Akash Kumar Singh <79398219+Afk-Akash@users.noreply.github.com>
2022-10-24 16:28:49 +0530

    Time: 240 ms (76.92%), Space: 44.6 MB (100.00%) - LeetHub

--- 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
@@ -1,30 +1,26 @@
+#define ll long long
 class Solution {
 public:
     long long minCost(vector<int>& nums, vector<int>& cost) {
-        int l = 1, r = 1e6, n = nums.size();
-        long long ans = 922337203685477580;
-        while( l  <= r ){
-            int mid1 = (l*2+r)/3, mid2 = (2*r+l)/3;
-            long long t1 = 0, t2 = 0;
-            
-            for(int i = 0; i < n; i++){
-                
-                int diff1 = abs(mid1 - nums[i]);
-                int diff2 = abs(mid2-nums[i]);
-                t1 += 1LL*diff1 * cost[i];
-                t2 += 1LL*diff2*cost[i];
+        int n = cost.size();
+        ll total = accumulate(cost.begin(), cost.end(), 1LL*0);
+        vector<pair<ll,ll>> v;
+        for(int i = 0; i < n ; i++)v.push_back({nums[i], cost[i]});
+        sort(v.begin(), v.end());
+        
+        ll mid = total/2, sum = 0 ;
+        ll target=0;
+        for(int i = 0; i < n; i++){
+            sum += v[i].second;
+            if(sum > mid){
+                target = v[i].first;
+                break;
             }
-            ans = min(ans, min(t1,t2));
-            // cout<<l<<" "<<r<<endl;
-            if(t1 < t2)r = mid2-1;
-            else l = mid1+1;
         }
-        long long t = LLONG_MAX;
-        // int mid = (l+r)/2;
-        // for(int i = 0; i < n; i++){
-        //     int diff = abs(nums[i] - mid);
-        //     t += 1LL * diff * cost[i];
-        // }
-        return min(ans, t);
+        ll ans = 0;
+        for(int i = 0; i < n; i++){
+            ans += abs(nums[i] - target) *1LL* cost[i];
+        }
+        return ans;
     }
 };
