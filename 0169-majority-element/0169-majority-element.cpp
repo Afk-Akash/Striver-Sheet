bd79629a595e5b83183ffbc847a1f179306f00ca
Akash Kumar Singh <79398219+Afk-Akash@users.noreply.github.com>
2022-10-26 12:23:10 +0530

    Time: 75 ms (5.38%), Space: 19.4 MB (99.58%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,15 +1,26 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int cnt = 0, val = 0, n = nums.size();
-        for(int i = 0; i < n; i++){
-            if(cnt == 0){
-                val = nums[i];
-                cnt++;
-            }
-            else if(nums[i] == val)cnt++;
-            else cnt--;
+        return fun(nums, 0, nums.size()-1);
+    }
+    
+    int fun(vector<int> &nums, int l, int r){
+        if(l== r) return nums[l];
+        
+        int mid = (l+r)/2;
+        int left = fun(nums, l, mid);
+        int right = fun(nums, mid+1, r);
+        
+        return freq(nums,l,r,left,right) ? left : right;
+    }
+    
+    bool freq(vector<int> &v,int l, int r, int j, int k){
+        int cnt = 0,cc=0;
+        for(int i = l; i <= r; i++){
+            int a = v[i];
+            if(a==k)cnt++;
+            if(a==j)cc++;
         }
-        return val;
+        return cc>cnt;
     }
 };
