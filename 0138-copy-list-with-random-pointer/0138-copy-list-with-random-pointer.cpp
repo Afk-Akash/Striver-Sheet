7afe9443df3caace2bb8d36e73f56a27c5f5615b
Akash Kumar Singh <79398219+Afk-Akash@users.noreply.github.com>
2022-11-01 18:49:56 +0530

    Time: 25 ms (8.06%), Space: 11.2 MB (59.92%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -1,43 +1,31 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    Node* next;
-    Node* random;
-    
-    Node(int _val) {
-        val = _val;
-        next = NULL;
-        random = NULL;
-    }
-};
-*/
+
 
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        unordered_map<Node*, Node*> mp;
-        Node* dummy = new Node(0), *p = head;
-        Node * ptr = dummy;
-        while(head){
-            Node * t  = new Node(head -> val);
-            ptr -> next = t;
-            mp[head] = t;
-            ptr = ptr -> next;
-            head = head -> next;
+        Node * p = head;
+        while(p){
+            Node * t = new Node(p -> val);
+            t -> next = p -> next;
+            p -> next = t;
+            p = p -> next -> next;
         }
-        mp[NULL] = NULL;
-        head = p;
-        Node* newHead = dummy -> next;
-        while(head){
-     
-            auto ptr = mp[head -> random];
-            newHead -> random = ptr;
-            head = head -> next;
-            newHead = newHead -> next;
+        p = head;
+        while(p){
+            auto t = p -> random;
+            if(t){
+                p-> next -> random = t -> next;
+            }
+            p = p -> next -> next;
         }
-
+        p = head;
+        Node * dummy = new Node(0), *temp = dummy;
+        while(p){
+            temp -> next = p -> next;
+            p -> next = p -> next -> next;
+            p = p -> next;
+            temp = temp -> next;
+        }
         return dummy -> next;
     }
 };
