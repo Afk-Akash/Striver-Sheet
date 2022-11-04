4253f65776f9573de5f7606e3b9e605c522fc0ca
Akash Kumar Singh <79398219+Afk-Akash@users.noreply.github.com>
2022-11-04 22:50:17 +0530

    Time: 13 ms (22.94%), Space: 11.6 MB (24.33%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        set<vector<int>> ans;
+        vector<vector<int>> ans;
         vector<int> temp;
         fun(0, nums, temp, ans);
-        vector<vector<int>> v;
-        for(auto & a : ans)v.push_back(a);
-        return v;
+        set<vector<int>> v;
+        for(auto & a : ans){sort(a.begin(), a.end());v.insert(a);}
+        ans.resize(0);
+        for(auto & a : v)ans.push_back(a);
+        return ans;
     }
     
-    void fun(int ind, vector<int> & nums, vector<int> temp, set<vector<int>> & ans){
+    void fun(int ind, vector<int> & nums, vector<int>& temp, vector<vector<int>> & ans){
         if(ind == nums.size()){
-            sort(begin(temp), temp.end());
-            ans.insert(temp);
+            // sort(begin(temp), temp.end());
+            ans.push_back(temp);
             return ;
         }
         
