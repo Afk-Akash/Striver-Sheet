90330145efa1269ff61e74e805f6bdc062f4f268
Akash Kumar Singh <79398219+Afk-Akash@users.noreply.github.com>
2022-11-05 17:19:36 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (70.89%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> temp;
-        fun(0, nums, temp, ans);
-        set<vector<int>> v;
-        for(auto & a : ans){sort(a.begin(), a.end());v.insert(a);}
-        ans.resize(0);
-        for(auto & a : v)ans.push_back(a);
-        return ans;
-    }
-    
-    void fun(int ind, vector<int> & nums, vector<int>& temp, vector<vector<int>> & ans){
-        if(ind == nums.size()){
-            // sort(begin(temp), temp.end());
-            ans.push_back(temp);
-            return ;
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        vector<vector<int>> ans = {{}};
+        int size = 0;
+        for(int i = 0; i < n; i++){
+            int start = i > 0 and nums[i] == nums[i-1] ? size : 0 ;
+            size = ans.size();
+            for(int j = start; j < size; j++){
+                auto t = ans[j];
+                t.push_back(nums[i]);
+                ans.push_back(t);
+            }
         }
-        
-        fun(ind + 1, nums, temp, ans);
-        temp.push_back(nums[ind]);
-        fun(ind + 1, nums, temp, ans);
-        temp.pop_back();
+        return ans;
     }
 };
