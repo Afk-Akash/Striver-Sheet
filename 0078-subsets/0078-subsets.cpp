797ace1670993f9b6c284487186127c64a2550fa
Akash Kumar Singh <79398219+Afk-Akash@users.noreply.github.com>
2022-11-05 17:33:13 +0530

    Time: 3 ms (77.60%), Space: 7.2 MB (57.16%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans = {{}};
         int n = nums.size();
-        for(int i = 0; i < n; i++){
-            int size = ans.size();
-            for(int j = 0; j < size; j++){
-                auto t = ans[j];
-                t.push_back(nums[i]);
-                ans.push_back(t);
+        vector<vector<int>> ans;
+        for(int i = 0; i < (1 << n); i++){
+            vector<int> t;
+            for(int j = 0; j < n; j++){
+                if((i >> j) & 1)t.push_back(nums[j]);
             }
+            ans.push_back(t);
         }
         return ans;
     }
