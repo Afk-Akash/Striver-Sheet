733694918bfc0448f5233083780291afc1501898
Akash Kumar Singh <79398219+Afk-Akash@users.noreply.github.com>
2022-11-11 18:28:54 +0530

    Time: 14 ms (43.47%), Space: 10.6 MB (73.31%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<vector<int>> v;
-        // set<vector<int>> ans;
+        vector<vector<int>> ans;
         vector<int> ds;
-        int n = candidates.size();
         sort(candidates.begin(), candidates.end());
-        for(auto  a : candidates)cout<<a<<",";
-        fun(0, candidates, ds, v, target);
-        // for(auto & a : ans)v.push_back(a);
-        return v;
+        fun(0, candidates, target, ds, ans);
+        return ans;
     }
     
-    void fun(int ind, vector<int> &nums, vector<int> ds, vector<vector<int>> &ans, int  k){
-        if(ind == nums.size()){
-            if(!k)ans.push_back(ds);
-            return;
+    void fun(int ind, vector<int>& v, int k, vector<int> &ds, vector<vector<int>> & ans){
+        if(k == 0) {
+            ans.push_back(ds);
+            return ;
         }
-        //1,1,2,5,6,7,10
+        if(ind == v.size())return ;
         
-        if(nums[ind] <= k){
-            ds.push_back(nums[ind]);
-            fun(ind + 1, nums, ds, ans, k - nums[ind]);
+        for(int i = ind; i < v.size(); i++){
+            if(i != ind and v[i-1] == v[i])continue;
+            if(v[i] > k) break;
+            ds.push_back(v[i]);
+            fun(i+1, v, k - v[i], ds, ans);
             ds.pop_back();
         }
-        ind++;
-        while(ind < nums.size() and nums[ind-1] == nums[ind])ind++;
-
-        fun(ind, nums, ds, ans, k);
     }
 };
