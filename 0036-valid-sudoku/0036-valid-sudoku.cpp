d992788318da84e28660690d1ab217a60019e574
Akash Kumar Singh <79398219+Afk-Akash@users.noreply.github.com>
2022-11-23 19:23:27 +0530

    Time: 31 ms (81.77%), Space: 18.1 MB (72.48%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,34 +1,15 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        int m = 9, n = 9;
+        int vis1[9][9]={0}, vis2[9][9]={0}, vis3[9][9]={0};
         for(int i = 0; i < 9; i++){
-            unordered_set<char> set;
             for(int j = 0; j < 9; j++){
                 if(board[i][j] == '.')continue;
-                if(set.find(board[i][j]) != set.end()) return 0;
-                set.insert(board[i][j]);
-                // cout<<i<<" ";
-            }
-        }
-        // cout<<endl;
-        for(int i = 0; i < 9; i++){
-            unordered_set<char> set;
-            for(int j = 0; j < 9; j++){
-                if(board[j][i] == '.')continue;
-                if(set.find(board[j][i]) != set.end()) return 0;
-                set.insert(board[j][i]);
-                // cout<<i<<" ";
-            }
-        }
-        // cout<<endl;
-        for(int i = 0; i < 9; i++){
-            unordered_set<char> set;
-            for(int j = 0; j < 9; j++){
-                if(board[(i/3)*3+j/3][(i%3)*3 + j%3] == '.')continue;
-                if(set.find(board[(i/3)*3+j/3][(i%3)*3 + j%3]) != set.end()) return 0;
-                set.insert(board[(i/3)*3+j/3][(i%3)*3 + j%3]);
-                // cout<<i<<" ";
+                int val = board[i][j] - '0' - 1, box = (i/3)*3 + j/3;
+                if(vis1[i][val] or vis2[j][val] or vis3[box][val]) return false;
+                vis1[i][val]=1;
+                vis2[j][val]=1;
+                vis3[box][val]=1;
             }
         }
         return 1;
