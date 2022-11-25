09141872b33e8e5e47500a1aefc4ea3aef922b90
Akash Kumar Singh <79398219+Afk-Akash@users.noreply.github.com>
2022-11-25 19:29:57 +0530

    Time: 243 ms (41.77%), Space: 43 MB (41.48%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -2,20 +2,28 @@ class Solution {
     int mod = 1e9+7;
 public:
     int sumSubarrayMins(vector<int>& arr) {
-        stack<int> st;
         int n = arr.size();
-        vector<int> dp(n);
-        long long ans = 0;
-        for(int i = 0; i < n; i++){
-            while(st.size() and arr[st.top()] > arr[i])st.pop();
-            
-            int prev = st.size() ? st.top() : -1;
-            dp[i] = arr[i] * (i - prev);
-            if(prev != -1)dp[i] += dp[prev];
-            
-            ans = (ans + dp[i])%mod;
+        vector<pair<int,int>> se(n);
+        stack<int> st;
+        for(int i = 0; i < arr.size(); i++){
+            while(st.size() and arr[i] < arr[st.top()])st.pop();
+            se[i].first = st.size() ? st.top() : -1;
             st.push(i);
-            
+        }
+        while(st.size())st.pop();
+        
+        for(int i = n - 1; i >= 0; i--){
+            while(st.size() and arr[i] <= arr[st.top()])st.pop();
+            se[i].second = st.size() ? st.top() : n;
+            st.push(i);
+        }
+        // for(auto & a : se){
+        //     cout<<a.first<<" "<<a.second<<endl;
+        // }
+        
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            ans = (ans % mod + 1LL*(i - se[i].first)*(se[i].second - i)*arr[i]%mod)%mod;
         }
         return ans;
     }
