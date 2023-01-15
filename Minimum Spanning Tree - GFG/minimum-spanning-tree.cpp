3546de60878ea6c9494b170aa834b8bc3814b14d
Akash Kumar Singh <79398219+Afk-Akash@users.noreply.github.com>
2023-01-15 14:54:47 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -5,30 +5,60 @@ using namespace std;
 // } Driver Code Ends
 class Solution
 {
-	public:
-	//Function to find sum of weights of edges of the Minimum Spanning Tree.
+	
+	class disjointSet{
+	    vector<int> parent, rank;
+	    public:
+	    disjointSet(int n){
+	        rank.resize(n+1, 0);
+	        parent.resize(n+1);
+	        iota(parent.begin(), parent.end(), 0);
+	    }
+	    
+	    int findParent(int node){
+	        if(parent[node] == node) return node;
+	        
+	        return parent[node] = findParent(parent[node]);
+	    }
+	    
+	    void unionByRank(int a, int b){
+	        int parentA = findParent(a), parentB = findParent(b);
+	        if(parentA == parentB) return ;
+	        
+	        if(rank[parentA] < rank[parentB]){
+	            parent[parentA] = parentB;
+	        }else if(rank[parentA] > rank[parentB]){
+	            parent[parentB] = parentA;
+	        }else{
+	            parent[parentA] = parentB;
+	            rank[b]++;
+	        }
+	    }
+	};
+public: 
     int spanningTree(int V, vector<vector<int>> adj[])
     {
-        // code here
-        priority_queue< vector<int>, vector<vector<int>> , greater<vector<int>>> pq;
-        vector<bool> vis(V,false);
-        int mst = 0;
-        
-        pq.push({0,V-1,-1});
-        while(pq.size()){
-            auto t = pq.top();
-            pq.pop();
-            int cost = t[0], node = t[1], parent = t[2];
-            if(vis[node])continue;
-            mst += cost;
-            vis[node] = true;
-            for(auto edge : adj[node]){
-                int upcomingNode = edge[0], wt = edge[1];
-                if(vis[upcomingNode]) continue;
-                pq.push({wt, upcomingNode, node});
+        vector<tuple<int,int,int>> v;
+        for(int i = 0; i < V; i++){
+            for(auto ed : adj[i]){
+                v.push_back(make_tuple(ed[1], i, ed[0]));
             }
         }
-        return mst;
+        int ans = 0;
+        sort(v.begin(), v.end());
+        disjointSet ds(V);
+        // for(auto tup : v)cout<<get<1>(tup)<<" "<<get<2>(tup)<<endl;return 0;
+        for(int i = 0; i < v.size(); i += 1){
+            auto tup = v[i];
+            int node1 = get<1>(tup), node2 = get<2>(tup);
+            int p1 = ds.findParent(node1), p2 = ds.findParent(node2);
+            // cout<<node1<<" "<<p1<<"   |||   "<<node2<<" "<<p2<<endl;
+            if(p1 == p2)continue;
+            ans += get<0>(tup);
+            
+            ds.unionByRank(node1, node2);
+        }
+        return ans;
     }
 };
 
